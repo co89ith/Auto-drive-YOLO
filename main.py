46df518f880304e4c240ccc79b2ddb488a7902ee
co89ith <xinyuzeng9@gamil.com>
2021-08-11 09:36:58 +0200

    Up load main code

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import PIL
+from PIL import ImageFont, ImageDraw, Image
+import tensorflow as tf
+from tensorflow.python.framework.ops import EagerTensor
+
+from tensorflow.keras.models import load_model
+from yad2k.models.keras_yolo import yolo_head
+from yad2k.utils.utils import draw_boxes, get_colors_for_classes, scale_boxes, read_classes, read_anchors, preprocess_image
+
+%matplotlib inline
